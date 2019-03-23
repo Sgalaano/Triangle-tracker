10db63038677303749290f1a88d93ae1963d9436
sgalaano <sgalaano@gmail.com>
2019-03-23 10:13:52 +0300

    html changes

--- Triangle.js
+++ Triangle.js
@@ -1,18 +1,48 @@
-var h = parseFloat(prompt("Enter dimension of first side"));
-var s = parseFloat(prompt("Enter dimension of Second side"));
-var w = parseFloat(prompt("Enter dimension of Third side"));
-var sides = function(a,b,c){
-return( a+b>c && b+c>a && c+a>b)
+function findout() {
+var inp1=document.getElementById("data1").value;
+var inp2=document.getElementById("data2").value;
+var inp3=document.getElementById("data3").value;
+var h =parseInt(inp1);
+var s =parseInt(inp2);
+var w =parseInt(inp3);
+if (h === s && s === w && w === h){
+  alert("EQUILATERAL TRAIANGLE");
 }
-if (sides(h,s,w) && h===s && s===w){
-alert("This forms an Equilateral triangle.")
+else if (h === s && h !== w) {
+  alert("ISOSCELES TRIANGLE");
+}else if (h === w && h !== s) 
+}else if (h!== s && s !== w && h !== w && h + s > w && s + w > w)
+  alert("SCALENE TRIANGLE");
+}else if (h + s <= w) {
+  alert ("NOT A TRIANGLE");
+}else if (h + w <=s) {
+  alert ("NOT A TRIANGLE");
+}else if (s + w <=h)  {
+  alert ("NOT A TRIANGLE");
 }
-else if(sides(h,s,w) && h===s || h===w || w===s){
-alert ("This forms an isosceles triangle.")
+}function findout() {
+var inp1=document.getElementById("data1").value;
+var inp2=document.getElementById("data2").value;
+var inp3=document.getElementById("data3").value;
+var sideA=parseInt(inp1);
+var sideB=parseInt(inp2);
+var sideC=parseInt(inp3);
+if (sideA === sideB && sideB === sideC && sideC === sideA){
+  alert("EQUILATERAL TRAIANGLE");
 }
-else if(sides(h,s,w) && h!=w!=s){
-alert ("This forms a scarlene triangle " )
+else if (sideA === sideB && sideA !== sideC) {
+  alert("ISOSCELES TRIANGLE");
+}else if (sideA === sideC && sideA !== sideB) {
+  alert("ISOSCELES TRAIANGLE");
+}else if (sideB === sideC && sideB !== sideA) {
+  alert("ISOSCELES  TRAIANGLE");
+}else if (sideA !== sideB && sideB !== sideC && sideA !== sideC && sideA + sideB > sideC && sideB + sideC > sideA){
+  alert("SCALENE TRIANGLE");
+}else if (sideA + sideB <= sideC) {
+  alert ("NOT A TRIANGLE");
+}else if (sideA + sideC <=sideB) {
+  alert ("NOT A TRIANGLE");
+}else if (sideB + sideC <=sideA)  {
+  alert ("NOT A TRIANGLE");
 }
-else {
-alert("ERROR, INVALID MEASUREMENTS ")
 }
